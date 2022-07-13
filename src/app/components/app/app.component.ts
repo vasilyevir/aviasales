6d9092484c1ede9adf14ab70e03b525e9f3b8b1a
vasilyevir <69314942+vasilyevir@users.noreply.github.com>
2022-07-13 23:27:58 +0300

    add eslint & prettier

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup} from "@angular/forms";
-import { Subject} from "rxjs";
-import {Airflight} from "../../Types/Airflight";
-import {Filter} from "../../Types/Filter";
-
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { Airflight } from '../../Types/Airflight';
+import { Filter } from '../../Types/Filter';
 
 const airFlights: Array<Airflight> = [
   {
@@ -14,7 +13,7 @@ const airFlights: Array<Airflight> = [
     startTravel: '10:45',
     endTravel: '8.00',
     timeInTravel: '21ч 15м',
-    transfers: []
+    transfers: [],
   },
   {
     cost: 13400,
@@ -24,7 +23,7 @@ const airFlights: Array<Airflight> = [
     startTravel: '10:45',
     endTravel: '8.00',
     timeInTravel: '21ч 15м',
-    transfers: ['HKG', 'JNB']
+    transfers: ['HKG', 'JNB'],
   },
   {
     cost: 13400,
@@ -34,9 +33,9 @@ const airFlights: Array<Airflight> = [
     startTravel: '10:45',
     endTravel: '8.00',
     timeInTravel: '21ч 15м',
-    transfers: ['HKG']
+    transfers: ['HKG'],
   },
-]
+];
 
 const filters: Array<Filter> = [
   {
@@ -99,38 +98,42 @@ const filters: Array<Filter> = [
       },
     ],
   },
-]
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   airFlights: Array<Airflight> = airFlights;
+
   filters: Array<Filter> = filters;
+
   title = 'Aviasales';
+
   filterForm: FormGroup;
+
   subject: Subject<any> = new Subject<any>();
 
   constructor(private formBuilder: FormBuilder) {
     this.filterForm = this.formBuilder.group({
       amountTransfer: new FormControl([]),
       company: new FormControl('All'),
-      popularFilter: new FormControl('Cheaper')
+      popularFilter: new FormControl('Cheaper'),
     });
   }
 
   ngOnInit() {
     this.filterForm.get('company')?.valueChanges.subscribe((res) => {
       console.log(res);
-    })
+    });
     this.filterForm.get('amountTransfer')?.valueChanges.subscribe((res) => {
       console.log(res);
-    })
+    });
     this.filterForm.get('popularFilter')?.valueChanges.subscribe((res) => {
       console.log(res);
-    })
+    });
   }
 
   change(e: any, value: any, filterName: any) {
@@ -141,15 +144,16 @@ export class AppComponent implements OnInit {
   onCheckChange(event: any) {
     const formArray: any = this.filterForm.get('amountTransfer') as FormControl;
 
-    if(event.target.checked){
+    if (event.target.checked) {
       formArray.setValue([event.target.value, ...formArray.value]);
-    }
-    else{
+    } else {
       let i: number = 0;
+      const array = formArray.value;
 
       formArray.value.forEach((ctrl: any) => {
-        if(ctrl.value == event.target.value) {
-          formArray.removeAt(i);
+        if (ctrl == event.target.value) {
+          array.splice(i, 1);
+          formArray.setValue(array);
           return;
         }
 
